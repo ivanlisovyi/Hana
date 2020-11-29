255aa5f10fcf725160663c810f5c098f02c1a012
ivanlisovyi <lisovyi.ivan@gmail.com>
2020-11-29 20:21:51 +0100

    Initial commit

---
+++ Hana/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hana
+//
+//  Created by Lisovyi, Ivan on 29.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hana/HanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanaApp.swift
+//  Hana
+//
+//  Created by Lisovyi, Ivan on 29.11.20.
+//
+
+import SwiftUI
+
+@main
+struct HanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
